9135687d538ede5ef0d7d1d3f616fec1a806ec33
Aheal <alejandroalamina@hotmail.com>
2018-10-09 23:08:36 -0500

    Se agrego el script para el registro de un nuevo usuario, esta en raw falta adaptarlo con el controlador

--- BS/registro.php
+++ BS/registro.php
@@ -1,4 +1,16 @@
 <?php
 
+$temp = $_POST['registro'];
+
+include 'connection.php'; 
+
+$sql = "INSERT INTO Usuarios (Nombre,ApellidoPaterno,ApellidoMaterno,Correo_e,Telefono,Genero) 
+        VALUES ('John','Doe','Lapa','aheal@gmail.com','9931538502','Masculino')"; 
+if ($conn -> query($sql) === TRUE){
+    echo "New record created successfully"; 
+} else {
+    echo "Error".$sql."<br>".$conn->error;
+}        
+$conn -> close();
 
 ?>
